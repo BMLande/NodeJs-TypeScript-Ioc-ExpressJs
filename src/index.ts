990bc1ae1cedea68503e95e8906c1a521e2489fa
BMLande <landebm@gmail.com>
2020-10-03 17:39:13 +0530

    added initial commit

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+const app = express();
+const port = 3000;
+
+
+// default handler
+app.get("/", (req,res)=>{
+    res.send("Js To Ts demo")
+});
+
+
+
+
+app.listen(port ,()=>{
+    // tslint:disable-next-line:no-console
+    console.log(`Application started on port : ${port}`);
+})
